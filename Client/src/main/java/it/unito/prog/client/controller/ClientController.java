cce9d813d4eb0897f0d1cdc64588171ac782f83d
Michele Lorenzo <michele.lorenzo@edu.unito.it>
2021-12-11 16:15:32 +0100

    Create and test ActionListener in ClientController

--- Client/src/main/java/it/unito/prog/client/controller/ClientController.java
+++ Client/src/main/java/it/unito/prog/client/controller/ClientController.java
@@ -1,6 +1,31 @@
 package it.unito.prog.client.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class ClientController {
+
+    @FXML
+    private AnchorPane contentPanel;
+
+    @FXML
+    private Label userEmail;
+
+    @FXML
+    void onComposeButtonAction(ActionEvent event) {
+        System.out.println("Clicked compose button!");
+    }
+
+    @FXML
+    void onInboxButtonAction(ActionEvent event) {
+        System.out.println("Clicked inbox button!");
+    }
+
+    @FXML
+    void onSentButtonAction(ActionEvent event) {
+        System.out.println("Clicked sent button!");
+    }
 
 }
