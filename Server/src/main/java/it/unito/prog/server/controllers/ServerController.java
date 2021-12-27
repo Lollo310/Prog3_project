d5562bc5d09ae30d01636c146014e149063bfceb
Michele Lorenzo <michele.lorenzo@edu.unito.it>
2021-12-27 11:43:08 +0100

    upload and update Server

--- Server/src/main/java/it/unito/prog/server/controllers/ServerController.java
+++ Server/src/main/java/it/unito/prog/server/controllers/ServerController.java
@@ -1,16 +1,17 @@
 package it.unito.prog.server.controllers;
 
+import it.unito.prog.server.models.Server;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
 public class ServerController {
 
     @FXML
-    private ListView<?> logListView;
+    private ListView<String> logListView;
 
     @FXML
     void initialize() {
-
+        Server serverModel = new Server();
+        logListView.setItems(serverModel.getLog());
     }
-
 }
