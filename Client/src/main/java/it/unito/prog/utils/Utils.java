2546789b0cc29df3871f95b7a9eb3453bc4c0f7f
shishkeb0b <elisaliperottino@gmail.com>
2022-01-02 22:01:44 +0100

    Added htmlToText method.

--- Client/src/main/java/it/unito/prog/utils/Utils.java
+++ Client/src/main/java/it/unito/prog/utils/Utils.java
@@ -1,11 +1,13 @@
 package it.unito.prog.utils;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Utils {
 
-    public String htmlToText() {
-        return  null;
+    public static String htmlToText(String htmlCode) {
+        Document doc = Jsoup.parse(htmlCode);
+        return doc.body().text();
     }
 
 }
