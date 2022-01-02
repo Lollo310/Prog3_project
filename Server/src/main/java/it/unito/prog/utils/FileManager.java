e3740da8fb3e26df8fbb14323e309c8053d033c1
shishkeb0b <elisaliperottino@gmail.com>
2022-01-02 19:37:41 +0100

    Added methods initUserDir and deleteEmail.

--- Server/src/main/java/it/unito/prog/utils/FileManager.java
+++ Server/src/main/java/it/unito/prog/utils/FileManager.java
@@ -2,7 +2,13 @@ package it.unito.prog.utils;
 
 import it.unito.prog.models.Email;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,38 +16,74 @@ import java.nio.file.Paths;
 public class FileManager {
 
     private static final String cwd = Path.of("").toAbsolutePath().toString();
-    private static final String parentDir = Paths.get(cwd).getParent().getFileName().toString();
-    public static final String BASE_PATH = parentDir + File.separator + "users";
+    private static final String parentDir = Paths.get(cwd).getParent().toAbsolutePath().toString();
+    public static final String BASE_PATH = parentDir + File.separator + "users" + File.separator;
 
-    //misc
-    public static boolean userDirExists(String basePath, String username) {
-        Path path = Paths.get(basePath + ":\\" + username);
+    /**
+     * Checks if a user's directory already exists.
+     * @param username username
+     * @return true if the user's directory exists, false otherwise.
+     */
+    public static boolean userDirExists(String username) {
+        Path path = Paths.get(BASE_PATH + File.separator + username);
         return Files.exists(path);
     }
 
-    public static void initUserDir(String basePath, String username) {
+    /**
+     * Creates a user directory containing an inbox and a sent directories.
+     * @param username username
+     */
+    public static void initUserDir(String username) {
 
-        Path path = Paths.get(basePath + ":\\" + username + "\\Inbox\\..\\Sent\\");
+        Path user = Paths.get(BASE_PATH + username);
+        Path inbox = Paths.get(BASE_PATH + username + File.separator + "Inbox");
+        Path sent = Paths.get(BASE_PATH + username + File.separator + "Sent");
 
         try {
-            Files.createDirectories(path);
+            Files.createDirectories(user);
+            Files.createDirectories(inbox);
+            Files.createDirectories(sent);
         } catch (IOException e) {
             System.err.println("Failed to initialize user's directory - " + e);
         }
     }
 
-    //handle locks
-
-    //handle send
-    public static void readEmail() {}
-
-    public static void sendEmail(Email email, String basePath, String username) {
-        if(!userDirExists(basePath, username)) {
-            initUserDir(basePath, username);
-        }
+    public static void sendEmail(Email email) {
+        //wip
     }
 
-    //handle delete
-    public static void deleteEmail() {}
+
+    /*
+     * https://www.baeldung.com/java-lock-files
+     * https://stackoverflow.com/questions/128038/how-can-i-lock-a-file-using-java-if-possible
+     * https://www.tabnine.com/code/java/classes/java.nio.channels.FileLock
+     * https://stackoverflow.com/questions/8678384/java-locking-a-file-for-exclusive-access
+     * https://github.com/eugenp/tutorials/blob/master/core-java-modules/core-java-nio-2/src/main/java/com/baeldung/lock/FileLocks.java
+     */
+    public static void deleteEmail(Email email, String username) throws IOException {
+        File lockPath = new File(BASE_PATH + username + File.separator);
+        boolean isReceived = Files.exists(Paths.get(BASE_PATH + username + File.separator + "Inbox" + File.separator + email.getId() + ".txt"));
+        boolean isSent = Files.exists(Paths.get(BASE_PATH + username + File.separator + "Sent" + File.separator + email.getId() + ".txt"));
+
+        if (!(isReceived && isSent)) { //file is either sent or received
+            FileChannel channel = new RandomAccessFile(lockPath, "rw").getChannel();
+            FileLock lock = channel.lock();
+
+            try {
+                lock = channel.tryLock();
+                File forDeletion = isReceived ? new File(BASE_PATH + username + File.separator + "Inbox" + File.separator + email.getId() + ".txt") :
+                                                new File(BASE_PATH + username + File.separator + "Sent" + File.separator + email.getId() + ".txt");
+                if(!forDeletion.delete()) System.err.println("Failed to delete file.");
+
+            } catch(OverlappingFileLockException e) {
+                System.err.println("File is already locked in this thread or virtual machine - " + e);
+            } finally {
+                if(lock != null) lock.release();
+                channel.close();
+            }
+        } else {
+            //file not found, handle error
+        }
+    }
 
 }
